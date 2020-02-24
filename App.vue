20612f781e05b76a39046cf0f7ee1346a6d70670
Jason <hlbj105@qq.com>
2020-02-24 16:46:54 +0800

    初始化，新增首页

---
+++ App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.log('App Launch');
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
+	}
+};
+</script>
+
+<style>
+/* 解决头条小程序组件内引入字体不生效的问题 */
+/* #ifdef MP-TOUTIAO */
+@font-face {
+	font-family: uniicons;
+	src: url('/static/uni.ttf');
+}
+/* #endif */
+view,text,image,swiper,swiper-item,scroll-view  {
+	box-sizing: border-box;
+}
+page {
+	background-color: #e5e5e5;
+}
+</style>
+<style lang="scss">
+@import './static/css/iconfont.scss';
+.more {
+	line-height: 80rpx;
+	text-align: center;
+	color: $uni-text-color-grey;
+}
+</style>
